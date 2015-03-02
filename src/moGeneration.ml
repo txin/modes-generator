63a246e211c6bb428191034131823c82642dbf16
txin <titian.tang@gmail.com>
2015-03-02 13:04:35 +0000

    started from scratch

--- src/moGeneration.ml
+++ src/moGeneration.ml
@@ -2,16 +2,12 @@ open Core.Std
 open MoOps
 module MoInst = MoInstructions
 
-let gen f ?(pruning=true) init depth insts =
+let gen init depth insts =
   let blocks = ref [] in
   let process init block =
     Log.info "Trying [%s] [%s]"
       (MoInst.string_of_t_list init) (MoInst.string_of_t_list block);
-    let g = MoGraph.createNew init block in
-    if f g then
-      begin
-        Log.info "It works!";
-        blocks := block :: !blocks
-      end
+    let g = MoGraph.create init block in
+    blocks := block :: !blocks
   in
   !blocks
